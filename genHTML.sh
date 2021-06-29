01e6bb8189bfebd587b13db6d89cd8f256daa528
Lesha Sokolov <user@Leshas-MacBook-Pro.local>
2021-06-29 12:44:47 +0300

    replace

--- genHTML.sh
+++ genHTML.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# replace ip in /etc/hosts for domain jw-tower.org 
+# replace ip in /etc/hosts for domain jwtowerorg.github.io 
 
 #run
-wget -r -l 7 -p -nc -E https://jw-tower.org
-cd jw-tower.org
+wget -r -l 7 -p -nc -E https://jwtowerorg.github.io
+cd jwtowerorg.github.io
 find . -type f -print0 | xargs -0 sed -i 's/http:\/\/jw/https:\/\/jw/g'
 git commit -a -m "meow"
 git push origin master
